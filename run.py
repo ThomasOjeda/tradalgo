6b0dd09295a18d926de72e799435086f543125ed
ThomasOjeda <tojeda@alumnos.exa.unicen.edu.ar>
2023-11-23 11:56:43 -0300

    cleaning

--- run.py
+++ run.py
@@ -1,43 +1,23 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import datetime
-import os.path
-import sys
-
-import backtrader as bt
-
-from strategy import MyStrategy, CarefulSizer
+from strategy import MyStrategy
 
 
-def run():
-    cerebro = bt.Cerebro()
-
-    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, "datasets/orcl-1995-2014.txt")
-
-    data = bt.feeds.YahooFinanceCSVData(
-        dataname=datapath,
-        fromdate=datetime.datetime(2000, 1, 1),
-        todate=datetime.datetime(2000, 12, 31),
-        reverse=False,
-    )
-
-    cerebro.adddata(data)
-
-    cerebro.broker.setcash(10000.0)
-
-    cerebro.addsizer(CarefulSizer, startingValue=cerebro.broker.getvalue())
-
-    cerebro.broker.setcommission(commission=0.001)
-
+def run(
+    cerebro,
+    periodSmaShortBuy,
+    periodSmaLongBuy,
+    periodSmaShortSell,
+    periodSmaLongSell,
+    stopBuyingAt,
+    buyTimerLimit,
+):
     cerebro.addstrategy(
         MyStrategy,
-        periodSmaShortBuy=10,
-        periodSmaLongBuy=20,
-        periodSmaShortSell=20,
-        periodSmaLongSell=40,
-        stopBuyingAt=0.5,
-        buyTimerLimit=20,
+        periodSmaShortBuy=periodSmaShortBuy,
+        periodSmaLongBuy=periodSmaLongBuy,
+        periodSmaShortSell=periodSmaShortSell,
+        periodSmaLongSell=periodSmaLongSell,
+        stopBuyingAt=stopBuyingAt,
+        buyTimerLimit=buyTimerLimit,
     )
 
     print("Starting Portfolio Value: %.2f" % cerebro.broker.getvalue())
